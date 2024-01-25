ef1de829dcf45e9fcc45926286ffa453080c978c
Subhraneel Saha <125677043+TerminatorSS24@users.noreply.github.com>
2024-01-25 18:58:40 +0530

    DAY 25

---
+++ Atlassian/Day 5/Day25_Q3.c
@@ -0,0 +1,50 @@
+/*
+**********CONSTRUCT THE LONGEST NEW STRING**********
+You are given three integers x, y, and z.
+
+You have x strings equal to "AA", y strings equal to "BB", and z strings equal to "AB". You want to choose some (possibly all or none) of these strings and concatenate them in some order to form a new string. This new string must not contain "AAA" or "BBB" as a substring.
+
+Return the maximum possible length of the new string.
+
+A substring is a contiguous non-empty sequence of characters within a string.
+
+
+
+Example 1:
+
+Input: x = 2, y = 5, z = 1
+Output: 12
+Explanation: We can concactenate the strings "BB", "AA", "BB", "AA", "BB", and "AB" in that order. Then, our new string is "BBAABBAABBAB".
+That string has length 12, and we can show that it is impossible to construct a string of longer length.
+Example 2:
+
+Input: x = 3, y = 2, z = 2
+Output: 14
+Explanation: We can concactenate the strings "AB", "AB", "AA", "BB", "AA", "BB", and "AA" in that order. Then, our new string is "ABABAABBAABBAA".
+That string has length 14, and we can show that it is impossible to construct a string of longer length.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+int longestString(int x, int y, int z)
+{
+    int ret = 0;
+    int temp = 0;
+    if (x > y)
+    {
+        temp = x - y;
+        ret = y * 4;
+    }
+    else if (y > x)
+    {
+        ret = x * 4;
+        temp = y - x;
+    }
+    else
+        ret = x * 4;
+
+    ret += z * 2;
+    if (temp)
+        ret += 2;
+    return ret;
+}
